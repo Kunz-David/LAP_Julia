35ca03019ac1ebc10d7949fced410b1ca67d1aae
David-Kunz <epicfail.davidkunz@gmail.com>
2020-02-26 15:52:01 +0100

    Adding basic files.

---
+++ src/Get_filter_basis.jl
@@ -0,0 +1,11 @@
+module Get_filter_basis
+
+    """
+        Returns a filter basis array with filters of size 'filter_size'.
+        - filter_size can be an array.
+    """
+    function get_filter_basis(filter_size)
+        print(filter_size)
+    end
+
+end
---
+++ src/test.jl
@@ -0,0 +1,9 @@
+
+using PkgTemplates
+
+t = Template(user = "David Kunz", dir = "/Users/MrTrololord/Google_Drive/cvut/bakalarka/julia")
+
+generate("LAP_julia", t)
+
+
+println("ahoj")
