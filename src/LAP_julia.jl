92c1901afdc6b5fc721113d4ecff0f1bd365d64a
David-Kunz <epicfail.davidkunz@gmail.com>
2020-02-27 13:02:54 +0100

    Files generated by PkgTemplates

---
+++ src/LAP_julia.jl
@@ -0,0 +1,5 @@
+module LAP_julia
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using LAP_julia
+using Test
+
+@testset "LAP_julia.jl" begin
+    # Write your own tests here.
+end
