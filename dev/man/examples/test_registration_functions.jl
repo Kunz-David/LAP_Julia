02c13d9d49df83ca883266110849061d67e000a7
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-08-09 09:21:09 +0000

    build based on 0e23554

--- dev/man/examples/test_registration_functions.jl
+++ dev/man/examples/test_registration_functions.jl
@@ -1,5 +1,5 @@
 
-# # Test Registration Functions Showcase
+# # Test Registration Functions
 #
 #md # [![](https://mybinder.org/badge_logo.svg)](@__BINDER_ROOT_URL__/generated/example_placeholder.ipynb)
 #md # [![](https://img.shields.io/badge/show-nbviewer-579ACA.svg)](@__NBVIEWER_ROOT_URL__/generated/example_placeholder.ipynb)
